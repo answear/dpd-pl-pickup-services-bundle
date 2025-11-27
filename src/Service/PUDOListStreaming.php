09d8b99e6ab42f8d9b41db456ab09fd62b5c29f9
Lukasz Falda <lukasz.falda@wearco.pl>
2025-11-27 10:26:17 +0100

    PUDOListStreaming with less memmory consumptions

--- src/Service/PUDOListStreaming.php
+++ src/Service/PUDOListStreaming.php
@@ -6,10 +6,13 @@ namespace Answear\DpdPlPickupServicesBundle\Service;
 
 use Answear\DpdPlPickupServicesBundle\Exception\MalformedResponseException;
 use Answear\DpdPlPickupServicesBundle\Exception\ServiceException;
-use Answear\DpdPlPickupServicesBundle\ValueObject\Coordinates;
 use Answear\DpdPlPickupServicesBundle\ValueObject\PUDO;
 use GuzzleHttp\Client;
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Exception\ServerException;
+use GuzzleHttp\RequestOptions;
+use Psr\Http\Message\ResponseInterface;
 
 class PUDOListStreaming extends AbstractPUDOList
 {
@@ -23,9 +26,7 @@ class PUDOListStreaming extends AbstractPUDOList
         $this->client = $client ?? new Client(
             [
                 'base_uri' => $configProvider->url,
-                'http_errors' => false,
-                'timeout' => $configProvider->requestTimeout,
-                'stream' => true,
+                RequestOptions::TIMEOUT => $configProvider->requestTimeout,
             ]
         );
     }
@@ -35,39 +36,94 @@ class PUDOListStreaming extends AbstractPUDOList
      */
     protected function request(string $endpoint, array $params): iterable
     {
-        $params['key'] = $this->configProvider->key;
+        $response = $this->query($endpoint, $params);
 
-        $response = $this->client->request('GET', $endpoint, ['query' => $params]);
-        $stream = $response->getBody();
+        $localFile = null;
+        try {
+            $localFile = tempnam(sys_get_temp_dir(), 'DPD_pp_');
+            $this->saveToLocalFile($localFile, $response);
 
-        $xmlContent = '';
-        while (!$stream->eof()) {
-            $xmlContent .= $stream->read(8192);
-        }
+            $reader = \XMLReader::open($localFile);
 
-        $xml = @\simplexml_load_string($xmlContent);
-        if (false === $xml) {
-            throw new MalformedResponseException($xmlContent);
-        }
+            while ($reader->read()) {
+                if (\XMLReader::ELEMENT === $reader->nodeType && 'ERROR' === $reader->name) {
+                    $error = new \SimpleXMLElement($reader->readOuterXML());
+                    if ($error && isset($error->VALUE)) {
+                        throw new ServiceException((string) $error->VALUE, (int) $error['code']);
+                    }
+                }
 
-        if ($xml->ERROR) {
-            throw new ServiceException((string) $xml->ERROR->VALUE, (int) $xml->ERROR['code']);
-        }
+                if (\XMLReader::ELEMENT === $reader->nodeType && 'PUDO_ITEM' === $reader->name) {
+                    $pudoElement = new \SimpleXMLElement($reader->readOuterXML());
 
-        $reader = new \XMLReader();
-        $reader->XML($xmlContent);
-
-        while ($reader->read()) {
-            if ($reader->nodeType === \XMLReader::ELEMENT && $reader->name === 'PUDO_ITEM') {
-                $pudoXml = $reader->readOuterXml();
-                $pudoElement = @\simplexml_load_string($pudoXml);
-
-                if (false !== $pudoElement) {
                     yield $this->PUDOFactory->fromXmlElement($pudoElement);
                 }
             }
+
+            $reader->close();
+        } finally {
+            $this->deleteLocalFile($localFile);
+        }
+    }
+
+    private function query(string $endpoint, array $params): ResponseInterface
+    {
+        $params['key'] = $this->configProvider->key;
+        $response = null;
+        try {
+            $response = $this->client->request(
+                'GET',
+                $endpoint,
+                [
+                    RequestOptions::QUERY => $params,
+                    RequestOptions::STREAM => true,
+                    RequestOptions::HTTP_ERRORS => true,
+                ]
+            );
+        } catch (ServerException $exception) {
+            throw new MalformedResponseException($exception->getMessage(), $exception);
+        } catch (RequestException $exception) {
+            $response = $exception->getResponse();
+        } catch (\Throwable) {
         }
 
-        $reader->close();
+        if (null === $response) {
+            throw new MalformedResponseException('Empty response.');
+        }
+
+        return $response;
+    }
+
+    private function saveToLocalFile(string $localFile, ResponseInterface $response): void
+    {
+        $fileHandle = fopen($localFile, 'wb');
+
+        $stream = $response->getBody();
+        $chunkSize = 8192;
+        while (!$stream->eof()) {
+            $chunk = $stream->read($chunkSize);
+            if (empty($chunk)) {
+                break;
+            }
+            fwrite($fileHandle, $chunk);
+        }
+
+        fclose($fileHandle);
+
+        if (filesize($localFile) <= 0) {
+            throw new MalformedResponseException('Empty local xml file after request.');
+        }
+    }
+
+    private function deleteLocalFile(mixed $localFile): void
+    {
+        if (!is_string($localFile) || !file_exists($localFile)) {
+            return;
+        }
+
+        try {
+            unlink($localFile);
+        } catch (\Throwable) {
+        }
     }
 }
